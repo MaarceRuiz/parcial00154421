a9b561fbddff6c29f153078ed2e27e8e5a2a398d
MaarceRuiz <00154421@uca.edu.sv>
2023-05-22 15:27:39 -0600

    Elaboracion de vistas

---
+++ app/src/main/java/com/example/myapplication/ui/dog/DogInfoFRagment.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.ui.dog
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+class DogInfoFRagment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dog_info_f_ragment, container, false)
+    }
+
+
+
+}
